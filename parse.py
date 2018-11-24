3aa85f15b4a64f66ee994463ae33de187ce11df7
Abdulhafiz67 <43554997+Abdulhafiz67@users.noreply.github.com>
2018-11-23 23:19:50 -0700

    Add files via upload

---
+++ parse.py
@@ -0,0 +1,52 @@
+import re
+
+querylist =  []   
+queryeraser =  []
+
+    
+def main():
+    print("Please enter your query, type exit to quit.")
+    while True:
+        text = input()
+        text = text.strip(" ")
+        text = " "+text 
+        if text == "exit":
+            break
+        else:
+            parseQuery(text)
+            break
+        
+def parseQuery(text):
+    global querylist
+    dateRe = '(\ {1,}date\{0,}[<>]{0,1}[=]{0,1}\d{2})[/](\d{2})[/](\d{4}\ {0,}[a-zA-Z0-9-_]{0,})'
+    priceRe = '(\ {1,}price\ {0,}[<>]{0,1}[=]{0,1}\ {0,}[a-zA-Z0-9-_]{0,})'
+    locationRe = '(\ {1,}location\ {0,}[=]{0,1}\ {0,}[a-zA-Z0-9-_]{0,})'
+    catRe = '(\ {1,}cat\ {0,}[=]{0,1}\ {0,}[a-zA-Z0-9-_]{0,})'
+    captureQuery(dateRe, text)
+    captureQuery(locationRe, text)
+    captureQuery(priceRe, text)
+    captureQuery(catRe, text)
+    captureRest(text)
+    print("QueryList")
+    print(querylist)
+
+def captureQuery(Re, text):
+    global queryeraser, querylist
+    captures = re.findall(Re, text)
+    for i in captures:
+        queryeraser.append(i)
+        i = i.replace(" ","")
+        querylist.append(i)
+
+
+def captureRest(text):
+    global queryeraser, querylist
+    strre = '(^\w{0,1}[^<>=]$)'
+    for i in queryeraser:
+        text = text.replace(i,"")
+    text = text.split(" ")
+
+    for i in text:
+        querylist.append(i)
+        
+main() 
