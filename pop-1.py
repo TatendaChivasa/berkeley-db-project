aaf8fa4b68aae30074da3554dcc76723d358b887
Tatenda Chivasa <33791640+ttch501@users.noreply.github.com>
2018-11-26 01:07:18 -0700

    Add files via upload
    does the partial terms match

--- pop-1.py
+++ pop-1.py
@@ -1,37 +1,56 @@
 from bsddb3 import db
+import re
 
-adsDB = db.DB()
 termsDB = db.DB()
-pdatesDB = db.DB()
-priceDB = db.DB()
-adsDB.open('ad.idx',None,db.DB_HASH,db.DB_CREATE)
 termsDB.open('te.idx',None,db.DB_BTREE,db.DB_CREATE)
-pdatesDB.open('da.idx',None,db.DB_BTREE,db.DB_CREATE)
-priceDB.open('pr.idx',None,db.DB_BTREE,db.DB_CREATE)
     
-
-curs = pdatesDB.cursor()
+cursor = termsDB.cursor()
 
 while(True):
     name = input("Enter a Name to look up: ")
+    te = name
     if(name == "q"): #Termination Condition
-        break
+        break 
     
-    result = curs.set(name.encode("utf-8")) 
-    #In the presence of duplicate key values, result will be set on the first data item for the given key. 
-   
-    if(result != None):
-        print("List of descriptionns that match:")
-        print("Key: " + str(result[0].decode("utf-8")) + ", Value: " + str(result[1].decode("utf-8")))
+    term1 = re.compile("[A-Za-z0-9]+[/%]")
+    
+    if te != 0:
+        if(term1.match(te)):
+            firstterm = re.sub("%",'',te)
+            #print('firstterm', firstterm)
         
-        #iterating through duplicates:
-        dup = curs.next_dup()
-        while(dup != None):
-            print("key: " + str(dup[0].decode("utf-8")) + ", Value: " + str(dup[1].decode("utf-8")))
-            dup = curs.next_dup()
-    else:
-        print("No Entry Found.")
+            matching = False    
+            if firstterm != 0:
+                matching = True
+                firstterm1 = firstterm[:-1]
+                end = len(firstterm1)
+                n = firstterm[:end]
+            result = cursor.set_range(n.encode("utf-8"))
+            
+        
+            if(result != None):
+                print("List of found descriptions:")
+            
+                while(result != None):
+                    #Checking the end condition: If the student's name comes after(or equal to) Ending_Name
+                    if(str(result[0].decode("utf-8")[:end])!=n): 
+                        break
+                    
+                    print(str(result[0].decode("utf-8")) + ": " + str(result[1].decode("utf-8")))
+                    result = cursor.next() 
+            else:
+                print("No ranges were found")
+                
+        else:
+            secondterm = re.sub("%", '', te)
+            
+            #def searchdatabase(name,cursor,database) #shoul call the iterating function
+    
+            
+    NewSearch = input("Do you want to start a new search?(press y for yes) ")
+    if(NewSearch != "y"): #Termination Condition
+        break    
             
 
-curs.close()
-priceDB.close()
+cursor.close()
+termsDB.close()
